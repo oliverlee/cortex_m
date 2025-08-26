db73192e6fced1ba234cb3bb0a860290a1773d36
Oliver Lee <oliverzlee@gmail.com>
2025-08-26 16:53:31 -0500

    run bazel within a nix shell
    Add a `tools/bazel` wrapper to launch the Bazel process within a Nix
    shell. This shell establishes a PATH only using the Nix store to ensure
    that all inputs of the build are specified.
    
    Note that this shell is only used when building the `cortex_m` repo --
    not when using `cortex_m` as an external.
    
    In order to start the Nix shell, which then calls `bazelisk` from Nix,
    `bash` and `nix` must be in the PATH.
    
    https://github.com/bazelbuild/bazelisk?tab=readme-ov-file#ensuring-that-your-developers-use-bazelisk-rather-than-bazel
    
    Change-Id: Iefaed1786aed904215cb5fe3e1135b4a56f6d14f

---
+++ tools/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "flake defining the repo dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "aarch64-darwin"];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tools = with pkgs;
+          [
+            bash
+            bazelisk
+            coreutils
+            findutils
+            gnugrep
+            nix
+          ]
+          ++ lib.optionals stdenv.isDarwin [
+            darwin.cctools
+          ];
+      in {
+        default = pkgs.mkShellNoCC {
+          packages = tools;
+
+          shellHook = ''
+            export PATH="${pkgs.lib.makeBinPath tools}"
+          '';
+        };
+      }
+    );
+  };
+}
